2f93f99ecf4a546fee30c647bee7f7744814e063
mustafa0180 <76091704+mustafaENG19CS0180@users.noreply.github.com>
2020-12-17 13:39:03 +0530

    Update debug_exam.py

--- debug_exam.py
+++ debug_exam.py
@@ -50,8 +50,7 @@ def uniqueUpdate(data1, data2):
                 dupKeys[k] = [v1, v2]
                 # Remove (k, v1) from data1
                 del data1[k]
-            else:
-                # Add (k, v2) to data1
+               else: # Add (k, v2) to data1
                 data1[k] = v2
     # After processing all (k, v2) in
     # data2, return the dictionary
